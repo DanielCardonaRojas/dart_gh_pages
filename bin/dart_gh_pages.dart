29b13d5c179b69989ff5c6c895b962c54c98a685
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-24 10:31:30 -0500

    First commit

---
+++ bin/dart_gh_pages.dart
@@ -0,0 +1,5 @@
+import 'package:dart_gh_pages/dart_gh_pages.dart' as dart_gh_pages;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_gh_pages.calculate()}!');
+}
---
+++ test/dart_gh_pages_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_gh_pages/dart_gh_pages.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
